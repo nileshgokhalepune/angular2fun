c05a071e390e6df25eb65ef027872b01488d1933
ICG\ngokhal <nilesh.gokhale@ingramcontent.com>
2017-03-13 15:57:45 -0500

    minor change to bubble sort

--- app/bubblesort/bubblesort.component.ts
+++ app/bubblesort/bubblesort.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
 export class BubbleSort {
     private array: any = [4, 2, 10, 6, 3, 8];
     private sortedarray: any = [];
-
+    private unsortedValues: any;
     private sort() {
+
         this.sortedarray = [];
-        this.array.forEach((data:any) => this.sortedarray.push(data));
+        if (this.unsortedValues && this.unsortedValues.length > 1) {
+            this.unsortedValues.split(',').forEach((data: any) => this.sortedarray.push(parseInt(data)));
+        } else {
+            this.array.forEach((data: any) => this.sortedarray.push(data));
+        }
         for (var i = 0; i < this.sortedarray.length; i++) {
             for (var j = i; j < this.sortedarray.length; j++) {
                 if (this.sortedarray[i] > this.sortedarray[j]) {
